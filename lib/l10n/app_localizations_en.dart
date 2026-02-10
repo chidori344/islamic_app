9a6e3e898b1585029d6e578fa20bfe93cd4a29b9
islame elsaadany <mond123123@gmail.com>
2026-02-10 13:07:32 +0200

    Merge pull request #3 from chidori344/feature-quran-screen
    Feature hadeth screen
    and finished loclization for languages from arbic to english and vis versa.

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,52 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get app_title => 'Islami';
+
+  @override
+  String get quran => 'Quran';
+
+  @override
+  String get hadeth => 'Hadeth';
+
+  @override
+  String get sebha => 'Sebha';
+
+  @override
+  String get radio => 'Radio';
+
+  @override
+  String get sura_name => 'Sura name';
+
+  @override
+  String get hadeth_name => 'Hadeth name';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get english => ' English';
+
+  @override
+  String get arbic => 'Arabic ';
+
+  @override
+  String get light => 'Light';
+
+  @override
+  String get theme => 'Theme';
+
+  @override
+  String get dark => 'Dark';
+
+  @override
+  String get language => 'Language';
+}
